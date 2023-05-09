4aaa232472f135330c44f718067aeadc5409cf50
Md. Abdullah-Al-Bakee <mail.bakee@gmail.com>
2023-05-08 19:28:13 -0500

    reformat code

--- src/main/java/miu_fpp/IsBunker.java
+++ src/main/java/miu_fpp/IsBunker.java
@@ -2,23 +2,23 @@ package miu_fpp;
 
 public class IsBunker extends BaseTest implements FppTest {
 
-	public void test() {
-		runTest(11, 1);
-		runTest(22, 1);
-		runTest(8, 0);
-	}
-	
-	void runTest(int n, int expected) {
-		int actual = isBunker(n);
-		printResult(actual, expected, n);
-	}
-	
-	int isBunker(int n) {
-		int element = 1;
-		for(int i = 2; element < n; i++) {
-			element += (i - 1);
-		}
-		
-		return (element == n) ? 1 : 0;
-	}
+    public void test() {
+        runTest(11, 1);
+        runTest(22, 1);
+        runTest(8, 0);
+    }
+
+    void runTest(int n, int expected) {
+        int actual = isBunker(n);
+        printResult(actual, expected, n);
+    }
+
+    int isBunker(int n) {
+        int element = 1;
+        for (int i = 2; element < n; i++) {
+            element += (i - 1);
+        }
+
+        return (element == n) ? 1 : 0;
+    }
 }
--- src/main/java/miu_fpp/MainClass.java
+++ src/main/java/miu_fpp/MainClass.java
@@ -2,17 +2,17 @@ package miu_fpp;
 
 public class MainClass {
 
-	public static void main(String[] args) {
-		FppTest[] tests = new FppTest[] {
-				new IsFibonacci(),
-				new IsTwin(),
-				new IsComplete(),
-				new IsDual(),
-				new IsBunker(),
-				new IsHollow(),
-				new HasNValues()
-		};
-		
+    public static void main(String[] args) {
+        FppTest[] tests = new FppTest[] { 
+                new IsFibonacci(),
+                new IsTwin(),
+                new IsComplete(),
+                new IsDual(),
+                new IsBunker(),
+                new IsHollow(),
+                new HasNValues()
+            };
+
         for (FppTest fppTest : tests) {
             fppTest.test();
         }
